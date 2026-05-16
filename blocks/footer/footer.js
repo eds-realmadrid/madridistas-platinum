dc2bc5b85e75ad7cc1d19e13582ed58e0a490f9d
adeporra <adeporra@adobe.com>
2026-05-17 01:03:48 +0200

    Revert "revert: restore locale-specific nav/footer content and metadata-driven paths"
    This reverts commit 5f172ce3a94158b0df376f6f167fcf0f5b699e3f.

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,4 +1,3 @@
-import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
 
 /**
@@ -6,9 +5,7 @@ import { loadFragment } from '../fragment/fragment.js';
  * @param {Element} block The footer block element
  */
 export default async function decorate(block) {
-  const footerMeta = getMetadata('footer');
-  const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
-  const fragment = await loadFragment(footerPath);
+  const fragment = await loadFragment('/footer');
 
   // decorate footer DOM
   block.textContent = '';
